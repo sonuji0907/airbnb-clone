31844434e6d619e6504c146bde1543c882d23a62
sonuji0907 <81800603+sonuji0907@users.noreply.github.com>
2021-08-19 19:24:32 +0900

    before deleting useradmin pannel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUSerAdmin(admin.ModelAdmin):
+
+    """Custom User Admin"""
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = ("superhost", "language", "currency")
